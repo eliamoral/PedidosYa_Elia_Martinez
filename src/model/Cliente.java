ebb829bd9b3fbead7e5ae1235d8863925fef4c9b
Elia Martinez <elia.martinez231@comunidadunir.net>
2025-10-02 19:48:18 +0200

    primer paso

---
+++ src/model/Cliente.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String email;
+}
---
+++ src/model/Pedido.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Pedido {
+    private int id;
+    private int clienteId;
+    private String producto;
+    private int cantidad;
+}
