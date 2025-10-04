b9b30809bf10110f81b1dfe5baaa52cd2d825d1c
Elia Martinez <elia.martinez231@comunidadunir.net>
2025-10-04 17:43:09 +0200

    creadas las clases

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -5,4 +5,51 @@ public class Pedido {
     private int clienteId;
     private String producto;
     private int cantidad;
+
+    public Pedido() {
+    }
+
+    public Pedido(int id, int clienteId, String producto, int cantidad) {
+        this.id = id;
+        this.clienteId = clienteId;
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getClienteId() {
+        return clienteId;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Pedido{id=%d, clienteId=%d, nombre=%s, email=%s}", id, clienteId, producto, cantidad);
+    }
 }
